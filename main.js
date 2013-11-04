fca4751c34da8c3badcca032ed164cec25ab0860
Imam Kurniawan <geekzy@gmail.com>
2013-11-04 18:22:22 +0700

    fix to give name to anonymous callbacks

--- main.js
+++ main.js
@@ -1,68 +1,69 @@
 //#!/usr/bin/node
 var 
 //--------------- MODULES ---------------//
-	// import tmdb module
+    // import tmdb module
     tmdb = require('tmdbv3')
-	// import optimist module
+    // import optimist module
   , args = require('optimist').argv
     // import shelljs module
   , shell = require('shelljs')
-	// import request module
+    // import request module
   , request = require('request')
     // import cheerio module
   , cheerio = require('cheerio')
-  
+
 //--------------- CONSTANTS ---------------//
   , EMPTY_FN = function() {}
-	// tmdb api key
+    // tmdb api key
   , KEY = 'efe8c33f77907e883db9a8fb44d10dfb'
-	// up one directory
+    // up one directory
   , UP_DIR = '..'
-	// the parent directory to be processed
+    // the parent directory to be processed
   , DIR = args._[0]
-  
+
 //--------------- FUNCTIONS ---------------//
   , processMovie = EMPTY_FN
   , evaluateFile = EMPTY_FN
   , evaluateTitle = EMPTY_FN
-  , evaluateYear = EMPTY_FN;
+  , evaluateYear = EMPTY_FN
+  , processListFile = EMPTY_FN;
 
 String.prototype.isEmpty = function() {
-	return this.length === 0;
+    return this.length === 0;
 };
 
 processMovie = function(err, res) {
-	if (res.results.length === 0) { return; }
-	var movie = res.results[0]
+    if (res.results.length === 0) { return; }
+    var movie = res.results[0]
+
+      , id = movie.id
+      , title = movie.title
+      , poster = movie.poster_path
+      , rate = movie.vote_average;
 	  
-	  , id = movie.id
-	  , title = movie.title
-	  , poster = movie.poster_path
-	  , rate = movie.vote_average;
-	  
-	tmdb.movie.info(id, function(err, res) {
-		var IMDB_URL = 'http://www.imdb.com/title/'+ res.imdb_id +'/';
-		
-		request(IMDB_URL, function(error, response, body) {
-			if (!error && response.statusCode == 200) {
-				var $  = cheerio.load(body), imdb = {};
-				$('[itemprop]').filter('span,div')
-					.each(function(i, e) {
-						var $e = $(e);
-						
-						if ($e.is('[itemprop=ratingValue]')) {
-							imdb['rate'] = $(e).text();
-						} else if ($e.is('[itemprop=ratingCount]')) {
-							imdb['votes'] = $(e).text();
-						} else if ($e.is('[itemprop=director]')) {
-							imdb['director'] = $(e).find('[itemprop=name]').text();
-						}
-					});
-				console.log(title + ' ['+IMDB_URL+'] ' + imdb.rate+'/'+imdb.votes);
-				// TODO save to DB
-			}				
-		});
-	});
+    tmdb.movie.info(id, function movieInfo(err, res) {
+        var IMDB_URL = 'http://www.imdb.com/title/'+ res.imdb_id +'/';
+
+        request(IMDB_URL, function requestCallback(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var $  = cheerio.load(body), imdb = {};
+                $('[itemprop]').filter('span,div')
+                    .each(function eachElt(i, e) {
+                        var $e = $(e);
+
+                        if ($e.is('[itemprop=ratingValue]')) {
+                            imdb['rate'] = $(e).text();
+                        } else if ($e.is('[itemprop=ratingCount]')) {
+                            imdb['votes'] = $(e).text();
+                        } else if ($e.is('[itemprop=director]')) {
+                            imdb['director'] = $(e).find('[itemprop=name]').text();
+                        }
+                    });
+                console.log(title + ' ['+IMDB_URL+'] ' + imdb.rate+'/'+imdb.votes);
+                // TODO save movie details to DB
+            }				
+        });
+    });
 };  
 
 /**
@@ -71,67 +72,88 @@ processMovie = function(err, res) {
  */  
 evaluateFile = function(file) {
 		
-	var // extract year of the movie file
-		year = file.substr(file.search(/\[[0-9]{4}\]/)+1, 4)
-	  , title = file.substring(0, file.search(/\([0-9-]{4,9}\)/)).trim()
-	  , movie = /\.(mp4|mkv|avi)$/.test(file);
+    var // extract year of the movie file
+        year = file.substr(file.search(/\[[0-9]{4}\]/)+1, 4)
+      , title = file.substring(0, file.search(/\([0-9-]{4,9}\)/)).trim()
+      , movie = /\.(mp4|mkv|avi)$/.test(file);
 
-	if (movie && !title.isEmpty()) {
-		// search movie and scrap its information
-		tmdb.search.movie(title, processMovie);
-	}
+    if (movie && !title.isEmpty()) {
+        // search movie and scrap its information
+        tmdb.search.movie(title, processMovie);
+    }
 };
 
 evaluateTitle = function(title) {
 		
-	// enter title directory
-	if (shell.test('-d', title)) {
-		shell.cd(title);
-		// list title directory
-		shell.ls().forEach(function(file) {
-			// evaluate the movie file
-			evaluateFile(file);
-		});
-		
-		// going back up to title directory
-		shell.cd(UP_DIR);
+    // enter title directory
+    if (shell.test('-d', title)) {
+        shell.cd(title);
+        // list title directory
+        shell.ls().forEach(function eachFile(file) {
+            // evaluate the movie file
+            evaluateFile(file);
+        });
 
-	}
-	// no title directory
-	else {
-		evaluateFile(title);
-	}
+        // going back up to title directory
+        shell.cd(UP_DIR);
+
+    }
+    // no title directory
+    else {
+        evaluateFile(title);
+    }
 };
 
 evaluateYear = function(year) {
-	// start from year
-	if (/^[0-9-]{4,9}$/.test(year)) {
-		// going into year directory
-		shell.cd(year);
-		// list the year directory
-		shell.ls().forEach(function(title) {
-			// evaluate the movie title directory
-			evaluateTitle(title);
-		});
-		// going back up to main directory
-		shell.cd(UP_DIR);
-	}
-	// title or file
-	else {
-		// evaluate the movie title directory
-		evaluateTitle(year);
-	}
+    // start from year
+    if (/^[0-9-]{4,9}$/.test(year)) {
+        // going into year directory
+        shell.cd(year);
+        // list the year directory
+        shell.ls().forEach(function eachTitle(title) {
+            // evaluate the movie title directory
+            evaluateTitle(title);
+        });
+        // going back up to main directory
+        shell.cd(UP_DIR);
+    }
+    // title or file
+    else {
+        // evaluate the movie title directory
+        evaluateTitle(year);
+    }
+};
+
+processListFile = function(file) {
+    // TODO implement processing movie list file
+
+    // read file
+    // loop entry
+    // filter correct lines to process
+    // process valid entries using 
+    // tmdb.search.movie(title, processMovie);
 };
 
 if (!DIR) {
-    console.error('you didn\'t specify parent directory for its contents to be procesed');
+    console.error('you didn\'t specify root directory for its contents to be procesed');
     shell.exit(1);
 }
 
+// init TMBD API
 tmdb = tmdb.init(KEY);
 // die on error
 shell.config.fatal = true;
-// change to top working directory
-shell.cd(DIR);
-// list top working directory
-shell.ls().forEach(evaluateYear);
+
+// process directory based movie list
+if (shell.test('-d', DIR)) {
+    // change to top working directory
+    shell.cd(DIR);
+    
+    // list top working directory
+    shell.ls().forEach(evaluateYear);
+}
+// should be file based movie list
+else {
+    // TODO process movie list file
+    processListFile(DIR);
+}
